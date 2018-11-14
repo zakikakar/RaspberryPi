f1238a535943317a969447a974e1e2cae9ab413e
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-11-14 09:58:04 +0100

    nothign

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ green = (0,255,0)
 blue = (0,0,255)
 yellow = (255,255,0)
 white = (255,255,255)
+nothing = (0,0,0)
 navn = "ZAKI"
 
 while True:
-  sense.show_message(navn, scroll_speed=0.1, text_colour=white, back_colour=blue)
+  sense.show_message(navn, scroll_speed=0.1, text_colour=nothing, back_colour=blue)
   sense.clear(red)
